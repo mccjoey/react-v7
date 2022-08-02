0becffdf3ccc414c6f0b09f3e8cdf487172cf13a
José Sampaio <jose@pippa.com.br>
2022-08-02 12:49:47 -0300

    Configure ESLint with JSX

--- JS Tools/src/App.jsx
+++ JS Tools/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {render} from "react-dom";
-import Pet from "./Pet";
+import Pet from "./Pet.jsx";
 
 // const App = () => {
 //   return React.createElement("div", {}, [
